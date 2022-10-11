e8351ac6deff8280198db2281f7571698eb64e5d
Vasse <egilmonsas@gmail.com>
2022-10-11 20:15:42 +0200

    added user creation

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,6 @@
+#[macro_use]
+extern crate rocket;
+
 pub mod catchers;
 pub mod fairings;
 pub mod models;
--- src/models/mod.rs
+++ src/models/mod.rs
@@ -1,3 +1,6 @@
+use ammonia::Builder;
+use std::collections::hash_set::HashSet;
+
 pub mod bool_wrapper;
 pub mod our_date_time;
 pub mod pagination;
@@ -8,3 +11,10 @@ pub mod text_post;
 pub mod user;
 pub mod user_status;
 pub mod video_post;
+
+pub fn clean_html(src: &str) -> String {
+    Builder::default()
+        .tags(HashSet::new())
+        .clean(src)
+        .to_string()
+}
