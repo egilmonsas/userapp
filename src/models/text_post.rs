61969aad9ee400f7540df9b968532ec6f939eb61
Vasse <egilmonsas@gmail.com>
2022-10-20 21:41:45 +0200

    started posts

--- src/models/text_post.rs
+++ src/models/text_post.rs
@@ -1,8 +1,8 @@
 use super::post::Post;
 use crate::traits::DisplayPostContent;
-pub struct TextPost(Post);
+pub struct TextPost(pub Post);
 impl DisplayPostContent for TextPost {
-    fn raw_html() -> String {
-        todo!("will implement later")
+    fn raw_html(&self) -> String {
+        format!("<p>{}</p>", self.0.content)
     }
 }
